2062f30eccb5a6c2a5195f66ece4f67eec6a8bc8
yolaa <dimitriyola@gmail.com>
2021-04-20 10:26:20 +0700

    Initial Commit

---
+++ upgrade Skill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  upgrade Skill
+//
+//  Created by Dimitri Yola on 20/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
